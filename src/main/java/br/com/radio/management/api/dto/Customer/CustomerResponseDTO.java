3360a284160d3a063dc88d0c85e763f817258211
Cibele Martins <cibelemdev@gmail.com>
2024-09-12 20:29:51 -0300

    🩹 fix: response do Customer

--- src/main/java/br/com/radio/management/api/dto/Customer/CustomerResponseDTO.java
+++ src/main/java/br/com/radio/management/api/dto/Customer/CustomerResponseDTO.java
@@ -1,5 +1,17 @@
 package br.com.radio.management.api.dto.Customer;
 
+import java.util.List;
+
+import br.com.radio.management.api.domain.model.Advertisement;
+
 public class CustomerResponseDTO extends CustomerRequestDTO {
-    
+        private List<Advertisement> advertisements;
+
+        public List<Advertisement> getAdvertisements() {
+            return advertisements;
+        }
+
+        public void setAdvertisements(List<Advertisement> advertisements) {
+            this.advertisements = advertisements;
+        }
 }
